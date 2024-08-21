d919e0c5d52692ef967f89bb66fb887a6028dec2
AmanKumarDash <97535657+AmanKumarDash@users.noreply.github.com>
2024-08-21 23:11:47 +0530

    Update portfolio.js
    added a button for chaning the theme to dark mode and light mode

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -5,3 +5,18 @@ function togglemenu(){
         icon.classList.toggle("open");
 
 }
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+        const toggleButton = document.getElementById('dark-mode-toggle');
+        const body = document.body;
+        
+        toggleButton.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+    
+            // Toggle dark mode for all elements that need it
+            document.querySelectorAll('nav, a, .btn, .btn-color-1, .btn-color-2, .menu-links').forEach(element => {
+                element.classList.toggle('dark-mode');
+            });
+        });
+    });
+    
